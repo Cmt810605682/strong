840c867693fe772be2029b687338446b04b3c72f
Cmt <caomengtao@mac-mini.local>
2016-11-10 15:26:19 +0800

    add file

---
+++ strong/strong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  strong
+//
+//  Created by Cmt on 16/11/10.
+//  Copyright © 2016年 Cmt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
